fcc4ab0ed7b8bffab5063394b0cd6876d95856f3
mikescar <mike@hotelengine.com>
2018-07-25 16:42:58 -0600

    Add methods for guessing and opponents

--- lib/airdrop_engine/game.ex
+++ lib/airdrop_engine/game.ex
@@ -8,6 +8,9 @@ defmodule AirdropEngine.Game do
 
   def add_player(game, name) when is_binary(name), do: GenServer.call(game, {:add_player, name})
 
+  def guess_coordinate(game, player, row, col) when player in @players, do:
+    GenServer.call(game, {:guess_coordinate, player, row, col})
+
   def handle_call({:add_player, name}, _from, state) do
     with {:ok, rules} <- Rules.check(state.rules, :add_player)
     do
@@ -17,6 +20,33 @@ defmodule AirdropEngine.Game do
       |> reply_success(:ok)
     else
       :error -> {:reply, :error, state}
+    end
+  end
+
+  def handle_call({:guess_coordinate, player_key, row, col}, _from, state_data)
+  do
+    opponent_key = opponent(player_key)
+    opponent_board = player_board(state_data, opponent_key)
+
+    with {:ok, rules} <-
+           Rules.check(state_data.rules, {:guess_coordinate, player_key}),
+         {:ok, coordinate} <-
+           Coordinate.new(row, col),
+         {hit_or_miss, forested_island, win_status, opponent_board} <-
+           Board.guess(opponent_board, coordinate),
+         {:ok, rules} <-
+           Rules.check(rules, {:win_check, win_status})
+    do
+      state_data
+      |> update_board(opponent_key, opponent_board)
+      |> update_guesses(player_key, hit_or_miss, coordinate)
+      |> update_rules(rules)
+      |> reply_success({hit_or_miss, forested_island, win_status})
+    else
+      :error ->
+        {:reply, :error, state_data}
+      {:error, :invalid_coordinate} ->
+        {:reply, {:error, :invalid_coordinate}, state_data}
     end
   end
 
@@ -97,6 +127,9 @@ defmodule AirdropEngine.Game do
     %{player1: player1, player2: player2, rules: %Rules{}}
   end
 
+  defp opponent(:player1), do: :player2
+  defp opponent(:player2), do: :player1
+
   defp player_board(state, player), do: Map.get(state, player).board
 
   defp reply_success(state, reply) do
@@ -106,6 +139,12 @@ defmodule AirdropEngine.Game do
 
   defp update_board(state, player, board), do:
     Map.update!(state, player, fn player -> %{player | board: board} end)
+
+  defp update_guesses(state, player_key, hit_or_miss, coordinate) do
+    update_in(state[player_key].guesses, fn guesses ->
+      Guesses.add(guesses, hit_or_miss, coordinate)
+    end)
+  end
 
   defp update_player2_name(state, name), do: put_in(state.player2.name, name)
 
